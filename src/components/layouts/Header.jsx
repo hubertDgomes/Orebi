cbfa76ee466e0521231d4dc01e709940cb152ff8
Hubert Dhrubo Gomes <gomeshubert8@gmail.com>
2025-09-10 22:34:30 +0600

    Refactor Header component to include mobile navigation toggle functionality

--- src/components/layouts/Header.jsx
+++ src/components/layouts/Header.jsx
@@ -1,25 +1,54 @@
-import React from 'react'
-import Container from '../Container'
-import Images from '../Images'
-import Logo from '/src/assets/Logo.png'
-import { Link } from 'react-router-dom'
+import React, { useState } from "react";
+import Container from "../Container";
+import Images from "../Images";
+import Logo from "/src/assets/Logo.png";
+import { Link } from "react-router-dom";
+import { IoMdMenu } from "react-icons/io";
+import { RxCross2 } from "react-icons/rx";
+
 const Header = () => {
+  const [toggle, setToggle] = useState(false);
+  const showNav = () => {
+    setToggle(!toggle);
+  };
   return (
     <Container className={"py-[30px]"}>
-        <div className="flex items-center">
-            <Images src={Logo}/>
-            <div className="m-auto">
-                <ul className='flex gap-x-[40px] font-DM text-[16px] font-semibold'>
-                    <Link to={"/"}><li>Home</li></Link>
-                    <li>Shop</li>
-                    <li>About</li>
-                    <li>Contacts</li>
-                    <li>Journal</li>
-                </ul>
-            </div>
-        </div>
-    </Container>
-  )
-}
+      <div className="flex items-center mx-[20px] lg:mx-0">
+        <Images src={Logo} />
+        <div className="m-auto">
+          <ul className="sm:flex gap-x-[40px] font-DM text-[16px] font-semibold hidden">
+            <Link to={"/"}>
+              <li>Home</li>
+            </Link>
+            <li>Shop</li>
+            <li>About</li>
+            <li>Contacts</li>
+            <li>Journal</li>
+          </ul>
 
-export default Header
+          {toggle && (
+            <div className="transition-all duration-700 block sm:hidden items-start">
+              <ul>
+                <Link to={"/"}>
+              <li className="px-5 py-3 text-[20px] border-1">Home</li>
+            </Link>
+            <li className="px-5 py-3 text-[20px] border-1">Shop</li>
+            <li className="px-5 py-3 text-[20px] border-1">About</li>
+            <li className="px-5 py-3 text-[20px] border-1">Contacts</li>
+            <li className="px-5 py-3 text-[20px] border-1">Journal</li>
+              </ul>
+            </div>
+          )}
+        </div>
+        <div className="block sm:hidden">
+          <button onClick={showNav}>
+            {toggle ? <RxCross2 /> : <IoMdMenu/>}
+          </button>
+          
+        </div>
+      </div>
+    </Container>
+  );
+};
+
+export default Header;
