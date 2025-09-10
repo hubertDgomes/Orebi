8ea8449538a09a646651ddf11ff56f93fc066b14
Hubert Dhrubo Gomes <gomeshubert8@gmail.com>
2025-09-10 23:03:50 +0600

    Enhance Search component layout and styling for better responsiveness

--- src/components/layouts/Search.jsx
+++ src/components/layouts/Search.jsx
@@ -25,32 +25,32 @@ const Search = () => {
     <>
       <div className="bg-[#F5F5F3] pt-[30px]">
         <Container>
-          <div className="flex justify-between items-center relative">
+          <div className="flex justify-between items-center relative mx-[14px]">
             <div className="flex items-center gap-x-[10px]">
               <HiOutlineMenuAlt1
                 onClick={catHandler}
                 className="cursor-pointer"
               />
-              <p>Shop by Category</p>
+              <p className="text-[10px] sm:text-[16px]">Shop by Category</p>
               {showCat && (
-                <div className="absolute top-[50px] p-3 text-white bg-black">
+                <div className="absolute z-10 top-[40px] sm:top-[50px] p-0 sm:p-4 text-white bg-black w-[80px] sm:w-fit">
                   <ul>
-                    <li className="p-3 hover:font-bold hover:p-5 transition-all duration-300 cursor-pointer font-DM text-[14px]">
+                    <li className="p-1 sm:p-3 hover:font-bold sm:hover:p-5 transition-all duration-300 cursor-pointer font-DM text-[10px] sm:text-[14px]">
                       Accesories
                     </li>
-                    <li className="p-3 hover:font-bold hover:p-5 transition-all duration-300 cursor-pointer font-DM text-[14px]">
+                    <li className="p-1 sm:p-3 hover:font-bold sm:hover:p-5 transition-all duration-300 cursor-pointer font-DM text-[10px] sm:text-[14px]">
                       Furniture
                     </li>
-                    <li className="p-3 hover:font-bold hover:p-5 transition-all duration-300 cursor-pointer font-DM text-[14px]">
+                    <li className="p-1 sm:p-3 hover:font-bold sm:hover:p-5 transition-all duration-300 cursor-pointer font-DM text-[10px] sm:text-[14px]">
                       Electronics
                     </li>
-                    <li className="p-3 hover:font-bold hover:p-5 transition-all duration-300 cursor-pointer font-DM text-[14px]">
+                    <li className="p-1 sm:p-3 hover:font-bold sm:hover:p-5 transition-all duration-300 cursor-pointer font-DM text-[10px] sm:text-[14px]">
                       Clothes
                     </li>
-                    <li className="p-3 hover:font-bold hover:p-5 transition-all duration-300 cursor-pointer font-DM text-[14px]">
+                    <li className="p-1 sm:p-3 hover:font-bold sm:hover:p-5 transition-all duration-300 cursor-pointer font-DM text-[10px] sm:text-[14px]">
                       Bags
                     </li>
-                    <li className="p-3 hover:font-bold hover:p-5 transition-all duration-300 cursor-pointer font-DM text-[14px]">
+                    <li className="p-1 sm:p-3 hover:font-bold sm:hover:p-5 transition-all duration-300 cursor-pointer font-DM text-[10px] sm:text-[14px]">
                       Home appliances
                     </li>
                   </ul>
@@ -61,28 +61,28 @@ const Search = () => {
               <input
                 type="text"
                 placeholder="Search Products"
-                className="w-[600px] pr-[50px] py-[10px] px-[20px] bg-white"
+                className="w-[150px] sm:w-[600px] pr-[50px] py-[10px] px-[20px] bg-white sm:placeholder:text-[19px] placeholder:text-[10px]"
               />
-              <FaMagnifyingGlass className="absolute right-5 top-3.5 cursor-pointer" />
+              <FaMagnifyingGlass className="absolute right-5 top-[20px] sm:top-3.5 cursor-pointer text-[10px] sm:text-[20px]" />
             </div>
-            <div className="flex gap-x-[15px] items-center ">
+            <div className="flex gap-x-[15px] items-center">
               <div className="relative">
                 <FaUser onClick={handleLog} className="cursor-pointer" />
                 {showLog && (
-                  <div className="absolute top-[40px] right-0 w-[200px] text-center font-DM font-bold">
-                    <p className="text-white bg-black p-4">My Account</p>
-                    <p className="bg-white text-black p-4">Log Out</p>
+                  <div className="absolute top-[20px] sm:top-[40px] right-0 sm:w-[200px] w-[90px] text-center font-DM font-bold">
+                    <p className="text-white bg-black p-1 text-[10px] sm:text-[20px] cursor-pointer sm:p-4">My Account</p>
+                    <p className="bg-white text-black p-1 text-[10px] sm:text-[20px] cursor-pointer sm:p-4">Log Out</p>
                   </div>
                 )}
               </div>
-              <Link to={"/Cart"}><button><IoCartSharp className="text-[18px]" /></button></Link>
+              <Link to={"/Cart"}><button><IoCartSharp className="text-[18px] cursor-pointer" /></button></Link>
             </div>
           </div>
         </Container>
         <Images className={"m-auto w-full"} src={Banner} />
       </div>
       <Container className={"py-[20px]"}>
-          <div className="flex justify-between">
+          <div className="flex sm:flex-row flex-col text-center justify-between">
             <div className="flex items-center gap-x-[10px]">
               <p className="text-[25px]">2</p>
               <p className="font-DM text-[16px]"> Two years warranty</p>
