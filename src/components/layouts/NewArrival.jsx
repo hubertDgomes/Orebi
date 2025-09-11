f1955579a03d72927a1af8739b4636e0a3b1003e
Hubert Dhrubo Gomes <gomeshubert8@gmail.com>
2025-09-11 22:08:30 +0600

    Refactor NewArrival and Products components for improved responsiveness and layout adjustments

--- src/components/layouts/NewArrival.jsx
+++ src/components/layouts/NewArrival.jsx
@@ -31,12 +31,37 @@ const NewArrival = () => {
 
   var settings = {
     dots: false,
-    infinite: true,
+    infinite: false,
     speed: 500,
     slidesToShow: 4,
-    slidesToScroll: 1,
-    nextArrow: <SampleNextArrow />,
-    prevArrow: <SamplePrevArrow />,
+    slidesToScroll: 4,
+    initialSlide: 0,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
   };
 
   const [items, setItems] = useState([]);
