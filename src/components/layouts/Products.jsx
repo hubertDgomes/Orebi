c67388c4d058667969addef9d479a7d249983a8d
Hubert Dhrubo Gomes <gomeshubert8@gmail.com>
2025-09-12 10:40:44 +0600

    Add mobile menu functionality to Products component for improved user interaction

--- src/components/layouts/Products.jsx
+++ src/components/layouts/Products.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Images from "../Images";
 import sample from "/src/assets/sample.png";
 import { FaHeart } from "react-icons/fa";
@@ -9,6 +9,7 @@ import { addCart } from "../../features/addToCartSlice";
 
 
 const Products = ({ src, proName, proPrice }) => {
+  const [show , setShow] = useState(false)
   let dispatch = useDispatch();
   const sendToCart = () => {
     
@@ -21,11 +22,40 @@ const Products = ({ src, proName, proPrice }) => {
       })
     );
   };
+  const mobileMenu = (e)=>{
+    setShow(!show)
+  }
   return (
     <>
-      <div className="lg:w-[370px] sm:w-[230px] relative group mx-[20px]">
+      <div className="lg:w-[370px] sm:w-[230px] relative group mx-[20px]" onClick={mobileMenu}>
         <Images className={"bg-[#F9F9F9] m-auto w-full"} src={src} />
-        <div className="bg-white py-[10px] px-[30px] flex flex-col items-end absolute bottom-[50px] w-full translate-y-[20px] opacity-0 group-hover:translate-y-0 group-hover:opacity-100 transition-all duration-400">
+        {show &&
+        <div className="bg-white py-[10px] px-[30px] flex flex-col items-end absolute bottom-[50px] sm:hidden w-full" onClick={(e)=>e.stopPropagation()} >
+          <div className="flex items-center gap-x-[10px]">
+            <p className="font-DM text-[16px] hover:font-bold py-[10px] cursor-pointer">
+              Add to Wish List
+            </p>
+            <FaHeart />
+          </div>
+          <div className="flex items-center gap-x-[10px]">
+            <p className="font-DM text-[16px] hover:font-bold py-[10px] cursor-pointer">
+              Compare
+            </p>
+            <IoSync />
+          </div>
+          <div className="flex items-center gap-x-[10px]">
+            <button
+              onClick={sendToCart}
+              className="font-DM text-[16px] hover:font-bold py-[10px] cursor-pointer"
+            >
+              Add to Cart
+            </button>
+            <FaCartShopping />
+          </div>
+            
+        </div>
+        }
+        <div className="bg-white py-[10px] px-[30px] hidden sm:flex flex-col items-end absolute bottom-[50px] w-full translate-y-[20px] opacity-0 group-hover:translate-y-0 group-hover:opacity-100 transition-all duration-400">
           <div className="flex items-center gap-x-[10px]">
             <p className="font-DM text-[16px] hover:font-bold py-[10px] cursor-pointer">
               Add to Wish List
